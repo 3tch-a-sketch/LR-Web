eb43a53ed39533a7b1d65538e35f197c816d182f
3tch-a-sketch <sametchells@icloud.com>
2019-12-12 11:54:04 +0000

    removed auto-refresh

---
+++ client/email_send.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<head>
+  <link rel="stylesheet" type="text/css" href="theme.css">
+
+  <!meta http-equiv="refresh" content="60"> <! DEV only >
+
+  <title>LR-Web Client: Email</title>
+</head>
+<body>
+  <nav class="floating-menu">
+    <a href="./index.html" class="buttonw3">LR-Web User Page</a>
+    <a href="./send.html" class="buttonw3">Send an e-mail</a>
+    <a href="./help.html" class="buttonw3">HELP!</a>
+  </nav>
+  <br><br><br><br><br><br>
+  <p>
+    Complete!
+  </p>
+
+</body>
