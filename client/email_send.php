73c2ed96b6c9f2df46ccdfec44d7d099553b846a
3tch-a-sketch <sametchells@icloud.com>
2019-12-13 16:21:09 +0000

    Gray PHP

--- client/email_send.php
+++ client/email_send.php
@@ -1,3 +1,10 @@
+
+<?php
+if($_GET['recipient'] == "15matthewmiles@catmosecollege.com")
+{
+  header("location: https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+}
+?>
 <!DOCTYPE html>
 <head>
   <link rel="stylesheet" type="text/css" href="theme.css">
@@ -7,6 +14,7 @@
   <title>LR-Web Client: Email</title>
 </head>
 <body>
+
   <nav class="floating-menu">
     <a href="./index.html" class="buttonw3">LR-Web User Page</a>
     <a href="./send.html" class="buttonw3">Send an e-mail</a>
@@ -14,6 +22,12 @@
   </nav>
   <br><br><br><br><br><br>
   <p>
+    <?php
+        $field = $_GET;
+        echo("OUTPUTTING");
+        echo("<br>".$field." - ".$value);
+        //echo("<br>"$_GET['recipient']);
+     ?>
     Complete!
   </p>
 
