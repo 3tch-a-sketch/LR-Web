973ffb1f1160fc8b421fd3f015f7ced645510be0
3tch-a-sketch <sametchells@icloud.com>
2019-12-14 11:54:38 +0000

    php test

--- client/email_send.php
+++ client/email_send.php
@@ -25,8 +25,8 @@ if($_GET['recipient'] == "15matthewmiles@catmosecollege.com")
     <?php
         $field = $_GET;
         echo("OUTPUTTING");
-        echo("<br>".$field." - ".$value);
-        //echo("<br>"$_GET['recipient']);
+        //echo("<br>".$field." - ".$value);
+        echo("<br>"$_GET['recipient']);
      ?>
     Complete!
   </p>
